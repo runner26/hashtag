1a5fedefe03a0a75552c427c22e1632f4ec53379
Victor Idongesit <victoridongesit@gmail.com>
2018-02-13 22:14:19 +0100

    design signup page

--- Hashtag/index.js
+++ Hashtag/index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import TermsAndConditions from './views/TermsAndConditions';
+import SignUp from './views/SignUp';
 
-AppRegistry.registerComponent('Hashtag', () => TermsAndConditions);
+
+AppRegistry.registerComponent('Hashtag', () =>SignUp);
--- Hashtag/styles/termsAndConditions.js
+++ Hashtag/styles/termsAndConditions.js
@@ -4,7 +4,6 @@ export default StyleSheet.create({
   container: {
     alignItems: 'center',
     flexDirection: 'column',
-    backgroundColor: 'blue',
     flex: 1
   },
   splashImageView: {
